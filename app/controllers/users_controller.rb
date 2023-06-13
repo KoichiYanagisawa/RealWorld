2dd81e22ae81d97ee95cddd9fe1cff9952195087
Koichi Yanagisawa <tng62522@gmail.com>
2023-06-13 17:07:31 +0900

    QUEST step 2-1 complete!!!

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { user: @user.as_json(only: %i[username email token bio image]) }, status: :created
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
---
+++ db/migrate/20230613074405_add_fields_to_users.rb
@@ -0,0 +1,7 @@
+class AddFieldsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :token, :string
+    add_column :users, :bio, :text
+    add_column :users, :image, :string
+  end
+end
