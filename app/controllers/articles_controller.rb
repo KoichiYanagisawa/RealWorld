bf3b72081b812ecbdb1e0bd9ee9ab41b008b7617
Koichi Yanagisawa <tng62522@gmail.com>
2023-06-26 12:56:34 +0900

    例外を投げる場所を変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,6 @@ class ArticlesController < ApplicationController
   before_action :authorize_user, only: %i[update destroy]
 
   def show
-    raise "This is an error for testing Sentry"
     @article = Article.find_by(slug: params[:slug])
     if @article
       render json: { article: article_response(@article) }, status: :ok
@@ -26,6 +25,8 @@ class ArticlesController < ApplicationController
   end
 
   def update
+    raise "This is an error for testing Sentry"
+
     @article = Article.find_by(slug: params[:slug])
     if @article&.update(article_params)
       render json: { article: article_response(@article) }, status: :ok
