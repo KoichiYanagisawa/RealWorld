e4da82d62aa2a6e299b66f8cfc213b51b4c812fd
Koichi Yanagisawa <tng62522@gmail.com>
2023-06-26 11:33:04 +0900

    エラーを発生させてCentryが捕捉できるか確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'elb#ok'
   scope :api do
-    post '/users', to: 'users#create'
+    post '/users', to: 'users#creat'
     post '/users/login', to: 'sessions#create'
     post '/articles', to: 'articles#create'
     get '/articles/:slug', to: 'articles#show'
